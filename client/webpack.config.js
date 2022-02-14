1215b2d66d4891a7899bda3e7a8eda7a93f82170
bripap <bpappas302@gmail.com>
2022-02-14 11:42:03 -0500

    deleted files and readded trying to fix issue

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,72 +1,64 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
-const path = require("path");
-const { InjectManifest } = require("workbox-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
 module.exports = () => {
   return {
-    mode: "development",
+    mode: 'development',
     entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
-      database: "./src/js/database.js",
+      main: './src/js/index.js',
     },
-
     output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
+    devtool: "source-map",
     plugins: [
       new HtmlWebpackPlugin({
-        template: "./index.html",
-        title: "Jate Text Editor",
+        template: './index.html',
+        title: 'jate List'
       }),
-
-      new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
-      }),
-
+      new WorkboxPlugin.GenerateSW(),
       new WebpackPwaManifest({
-        name: "Jate - Text Editor",
-        short_name: "Jate",
-        description:
-          "Progressive Web Application Developed for UNH Coding Bootcamp 2022",
-        background_color: "#ffffff",
-        crossorigin: "use-credentials", //can be null, use-credentials or anonymous
-        start_url: "/",
-        publicPath: "/",
+        name: 'Jate - text editor',
+        short_name: 'Jate',
+        description: 'Keep track of important tasks!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
-            src: path.resolve("src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
-            destination: path.join("assets", "icons"),
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
           },
         ],
       }),
     ],
-    devtool: "source-map",
 
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
-              plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
-              ],
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
       ],
     },
-  };
-};
+  }
+}
